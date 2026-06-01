eacbac955a74c5f5a2ee321646edc4c463ba867c
Henry Amponsah <toqnoe@outlook.com>
2026-06-01 22:41:52 +0000

    feat: add timestamp to root route response

--- src/api/controllers/system/root.controller.js
+++ src/api/controllers/system/root.controller.js
@@ -1,6 +1,11 @@
 class RootController {
   static getAppInfo = (req, res) => {
-    res.status(200).json({ message: "Welcome to Agnon API" });
+    res
+      .status(200)
+      .json({
+        message: "Welcome to Agnon API",
+        timestamp: new Date().toISOString(),
+      });
   };
 }
 
