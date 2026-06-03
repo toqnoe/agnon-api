4130707898aca728d2aa1833afb8954de4b67828
Henry Amponsah <toqnoe@outlook.com>
2026-06-03 07:26:40 +0000

    refactor: update redis connection config to support development and production cases

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,14 +2,22 @@ import { createClient } from "redis";
 import env from "./env.js";
 import logger from "../api/utils/logger.js";
 
-const redis = createClient({
-  username: env.REDIS_USERNAME,
-  password: env.REDIS_PASSWORD,
-  socket: {
-    host: env.REDIS_HOST,
-    port: env.REDIS_PORT,
-  },
-});
+const redisConfig =
+  env.NODE_ENV === "production"
+    ? {
+        username: env.REDIS_USERNAME,
+        password: env.REDIS_PASSWORD,
+        socket: {
+          host: env.REDIS_HOST,
+          port: env.REDIS_PORT,
+        },
+      }
+    : {
+        host: "127.0.0.1",
+        port: 6379,
+      };
+
+const redis = createClient(redisConfig);
 
 redis.on("connect", () => {
   logger.info("Redis connecting...");
